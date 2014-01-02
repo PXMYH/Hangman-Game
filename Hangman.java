722d9f5d223c092b2a682a911a32aee5690e1076
S <s>
2014-01-02 22:18:38 +0800

    {"type":"TI", "runs":"2"}

--- Hangman.java
+++ Hangman.java
@@ -32,6 +32,7 @@ public class Hangman extends ConsoleProgram {
     	println("word index is " + word_index);
     	String guess_word = "hello";
     	//guess_word = hangmanlecxicon.getWord(word_index);
+    	guess_word = hangmanlecxicon.getWord(2);
     	println("the guess word is " + hangmanlecxicon.getWord(word_index));
     	
     	// constant check user guessing results and chances left 

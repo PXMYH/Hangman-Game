864f5a2d5def383275ac62e56e1e5e51b8b101fe
S <s>
2014-01-06 15:39:03 +0800

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -126,6 +126,7 @@ public class Hangman extends ConsoleProgram {
 					sub_after = guess_word.substring(index + 1, guess_word.length());
 				} else { // the letter is the last letter in the word
 					sub_before = guess_word.substring(0, index-1);
+					sub_after = "";
 				}
 			user_word = sub_before + ch + sub_after;
 		}

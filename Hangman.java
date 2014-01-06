6fcc6cca272613c99826210a418c115dd5c35395
S <s>
2014-01-06 14:51:32 +0800

    {"type":"TI", "runs":"1"}

--- Hangman.java
+++ Hangman.java
@@ -77,7 +77,7 @@ public class Hangman extends ConsoleProgram {
     // initalize word 
     private String initWord (String word) {
     	String tmp_word = "";
-    	for (int index = 0; index < tmp_word.length(); index ++){
+    	for (int index = 0; index < word.length(); index ++){
     		tmp_word = tmp_word + "-";
     	}
     	return tmp_word;

316790bc113360b8549079d8b9da97a46a6d9da6
S <s>
2014-01-06 14:24:48 +0800

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -38,7 +38,7 @@ public class Hangman extends ConsoleProgram {
     	println("the guess word is " + guess_word);
     	
     	user_word = initWord(user_word);
-    	user_word = "-----";
+    	//user_word = "-----";
     	
     	// constant check user guessing results and chances left 
     	while (number_of_chances > 0 && isResMatch() != true) {

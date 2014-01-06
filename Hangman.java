1e4d3057bfbeea6128983ef1779930f632361776
S <s>
2014-01-06 14:57:22 +0800

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -106,6 +106,7 @@ public class Hangman extends ConsoleProgram {
 	private void flipLetter(String letter) {
 		String tmp_word = guess_word;
 		tmp_word.replaceAll(letter, "-");
+		println();
 		 
 	}
 

99451addf0a31646c846f26db8b3f74223664c36
S <s>
2014-01-06 15:07:48 +0800

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -104,7 +104,7 @@ public class Hangman extends ConsoleProgram {
 	
 	// flip the all letters user guessed correctly
 	private void flipLetter(String letter) {
-		String tmp_word = guess_word, another;
+		String tmp_word = guess_word, return_word;
 		another = tmp_word.replaceAll("E", "-");
 		println("Flip word now is " + another);
 		 

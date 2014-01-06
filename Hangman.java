1bccfe11a30db8833526269601447c920b49fcd7
S <s>
2014-01-06 15:41:52 +0800

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -119,7 +119,7 @@ public class Hangman extends ConsoleProgram {
 			// find match in specific position
 			if (guess_word.charAt(index) == ch) {
 				if (index != 0 && index != guess_word.length()) { // the letter is not the first one nor the last one
-					sub_before = guess_word.substring(0, index-1);
+					sub_before = user_word.substring(0, index-1);
 					sub_after = user_word.substring(index+1, guess_word.length());
 				} else if (index == 0) { // the letter is the first letter in the word
 					sub_before = "";

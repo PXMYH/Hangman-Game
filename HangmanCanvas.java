42cc14ff46bcb1c7c5b6d40740610ad79529c4be
S <s>
2014-01-07 23:41:16 +0800

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -180,6 +180,7 @@ public class HangmanCanvas extends GCanvas {
 		
 		left_upper_arm = new GLine(pt_f_x, pt_f_y, pt_g_x, pt_g_y);
 		left_lower_arm = new GLine(pt_g_x, pt_g_y, pt_h_x, pt_h_y);
+		
 		add(left_upper_arm);
 		add(left_lower_arm);
 	}
@@ -195,6 +196,8 @@ public class HangmanCanvas extends GCanvas {
 		
 		right_upper_arm = new GLine(pt_f_x, pt_f_y, pt_i_x, pt_i_y);
 		right_lower_arm = new GLine(pt_i_x, pt_i_y, pt_j_x, pt_j_y);
+		
+		add()
 		
 	}
 	

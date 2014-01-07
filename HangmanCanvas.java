b068bfebc4a32d6757bbd19a86e911470b875307
S <s>
2014-01-07 11:55:24 +0800

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -40,7 +40,7 @@ public class HangmanCanvas extends GCanvas {
 	private double pt_g_x;
 	private double pt_g_y;
 	private double pt_h_x;
-	private double pt_h_y = ;
+	private double pt_h_y;
 	private final double pt_j_x = ;
 	private final double pt_j_y = ;
 	private final double pt_k_x = ;

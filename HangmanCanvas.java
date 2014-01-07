69b04dd2ee6fcd38fe2c53c6fdb2d7c0dd668f4a
S <s>
2014-01-07 11:18:36 +0800

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -32,7 +32,7 @@ public class HangmanCanvas extends GCanvas {
 	private final double pt_c_x = getWidth() / 2;
 	private final double pt_c_y = pt_b_y;
 	private final double pt_d_x = pt_c_x;
-	private final double pt_d_y = pt_x_y + ROPE_LENGTH;
+	private final double pt_d_y = pt_c_y + ROPE_LENGTH;
 	private final double pt_e_x = ;
 	private final double pt_e_y = ;
 	private final double pt_f_x = ;

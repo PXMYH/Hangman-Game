051c4b0e4c32a4eb97531a70cfd0cf2fc002aba9
S <s>
2014-01-07 11:17:02 +0800

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -60,7 +60,7 @@ public class HangmanCanvas extends GCanvas {
 	public void reset() {
 		// draw scaffold
 		scaffold = new GLine(pt_a_x, pt_a_y, pt_b_x, pt_b_y);
-		beam = new GLine(getWidth() / 2 - BEAM_LENGTH);
+		beam = new GLine(pt_b_x, pt_b_y);
 		add(scaffold);
 	}
 

ddd42419c4a4357308ad5b0c544e7bbfbd918d8b
S <s>
2014-01-07 23:45:46 +0800

    {"type":"TI", "runs":"0"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -204,7 +204,7 @@ public class HangmanCanvas extends GCanvas {
 	private void drawLeftLeg() {
 		// left leg + half hip
 		pt_l_x = pt_k_x + HIP_WIDTH / 2 ;
-		pt_l_y = pt_k_u;
+		pt_l_y = pt_k_y;
 		pt_m_x = ;
 		pt_m_y = ;
 	}

20350803ec52b6be99e6b241b558b95a2ae9bed7
S <s>
2014-01-12 19:56:25 -0800

    {"type":"TI", "runs":"1"}

--- Hangman.java
+++ Hangman.java
@@ -57,7 +57,7 @@ public class Hangman extends ConsoleProgram {
     	int high_boundary = hangmanlecxicon.getWordCount();
     	int word_index = (int) rgen.nextDouble(0, high_boundary);
     	guess_word = hangmanlecxicon.getWord(word_index);
-    	println("guessed word is: " + guess_word);
+    	println("guessed word is: " + guess_word + "num of words are: " + high_boundary);
     	
     	user_word = initWord(guess_word.length());
     	
